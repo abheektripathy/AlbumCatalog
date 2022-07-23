80c277b4b0661c78ebe0a856fd5eecd07c6d2fd5
Abheek Tripathy <90976669+abheektripathy@users.noreply.github.com>
2022-07-23 13:09:11 +0530

    comp

--- lib/Widgets/item_widget.dart
+++ lib/Widgets/item_widget.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, camel_case_types
 
 import 'package:catalog_app/Albums/catalog.dart';
 import 'package:flutter/material.dart';
